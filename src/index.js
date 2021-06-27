5120ff2cc0d88977d9384137d2f001f9faa6502b
SebastianC <sebas99cano@gmail.com>
2021-06-26 19:19:40 -0500

    herencia de componentes

--- src/index.js
+++ src/index.js
@@ -2,98 +2,66 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-function BoilingVerdict(props) {
-    if (props.celsius >= 100) {
-        return <p>The water would boil.</p>;
-    }
-    return <p>The water would not boil.</p>;
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+function Dialog(props) {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                {props.title}
+            </h1>
+            <p className="Dialog-message">
+                {props.message}
+            </p>
+            {props.children}
+        </FancyBorder>
+    );
 }
 
-class Calculator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-        this.state = {temperature: '', scale: 'c'};
-    }
-
-    handleCelsiusChange(temperature) {
-        this.setState({scale: 'c', temperature});
-    }
-
-    handleFahrenheitChange(temperature) {
-        this.setState({scale: 'f', temperature});
-    }
-
-    render() {
-        const scale = this.state.scale;
-        const temperature = this.state.temperature;
-        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-
-        return (
-            <div>
-                <TemperatureInput
-                    scale="c"
-                    temperature={celsius}
-                    onTemperatureChange={this.handleCelsiusChange} />
-                <TemperatureInput
-                    scale="f"
-                    temperature={fahrenheit}
-                    onTemperatureChange={this.handleFahrenheitChange} />
-                <BoilingVerdict
-                    celsius={parseFloat(celsius)} />
-            </div>
-        );
-    }
-}
-const scaleNames = {
-    c: 'Celsius',
-    f: 'Fahrenheit'
-};
-
-class TemperatureInput extends React.Component {
+class SignUpDialog extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        this.props.onTemperatureChange(e.target.value);
+        this.handleSignUp = this.handleSignUp.bind(this);
+        this.state = {login: ''};
     }
 
     render() {
-        const temperature = this.props.temperature;
-        const scale = this.props.scale;
         return (
-            <fieldset>
-                <legend>Enter temperature in {scaleNames[scale]}:</legend>
-                <input value={temperature}
+            <Dialog title="Mars Exploration Program"
+                    message="How should we refer to you?">
+                <input value={this.state.login}
                        onChange={this.handleChange} />
-            </fieldset>
+                <button onClick={this.handleSignUp}>
+                    ¡Apúntame!
+                </button>
+            </Dialog>
         );
     }
-}
 
-function toCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
-}
-
-function toFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
-}
-
-function tryConvert(temperature, convert) {
-    const input = parseFloat(temperature);
-    if (Number.isNaN(input)) {
-        return '';
+    handleChange(e) {
+        this.setState({login: e.target.value});
     }
-    const output = convert(input);
-    const rounded = Math.round(output * 1000) / 1000;
-    return rounded.toString();
+
+    handleSignUp() {
+        alert(`Bienvenido abordo, ${this.state.login}!`);
+    }
+}
+
+function WelcomeDialog() {
+    return (
+        <Dialog
+            title="Welcome"
+            message="Thank you for visiting our spacecraft!" />
+    );
 }
 ReactDOM.render(
-    <Calculator />,
+    <WelcomeDialog />,
     document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
