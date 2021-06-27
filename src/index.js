a2ed659c3f08ffa42225268abd0102623ffa12cf
SebastianC <sebas99cano@gmail.com>
2021-06-26 21:50:57 -0500

    use efect

--- src/index.js
+++ src/index.js
@@ -2,31 +2,24 @@ import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-function Example() {
-    const [count, setCount] = useState(0);
+class Example extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
 
-    // Similar a componentDidMount y componentDidUpdate:
-    useEffect(() => {
-        // Actualiza el título del documento usando la Browser API
-        document.title = `You clicked ${count} times`;
-    });
-
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
-            </button>
-        </div>
-    );
-}
-
-function ExampleWithManyStates() {
-    // Declarar múltiple variables de estado!
-    const [age, setAge] = useState(42);
-    const [fruit, setFruit] = useState('banana');
-    const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
-    // ...
+    render() {
+        return (
+            <div>
+                <p>You clicked {this.state.count} times</p>
+                <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+                    Click me
+                </button>
+            </div>
+        );
+    }
 }
 ReactDOM.render(
     <Example />,
