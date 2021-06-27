d936c679919c559d2e5a1abb9784cc2714f84f9a
SebastianC <sebas99cano@gmail.com>
2021-06-26 19:05:57 -0500

    formulario

--- src/index.js
+++ src/index.js
@@ -4,87 +4,43 @@ import reportWebVitals from './reportWebVitals';
 
 import App from "./App";
 
-function UserGreeting(props) {
-    return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-    return <h1>Please sign up.</h1>;
-}
-function LoginButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Login
-        </button>
-    );
-}
-
-function LogoutButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Logout
-        </button>
-    );
-}
-function Greeting(props) {
-    const isLoggedIn = props.isLoggedIn;
-    if (isLoggedIn) {
-        return <UserGreeting />;
-    }
-    return <GuestGreeting />;
-}
-
-function Mailbox(props) {
-    const unreadMessages = props.unreadMessages;
-    return (
-        <div>
-            <h1>Hello!</h1>
-            {unreadMessages.length > 0 &&
-            <h2>
-                You have {unreadMessages.length} unread messages.
-            </h2>
-            }
-        </div>
-    );
-}
-
-const messages = ['React', 'Re: React', 'Re:Re: React'];
-ReactDOM.render(
-    <Mailbox unreadMessages={messages} />,
-    document.getElementById('root')
-);
-
-class LoginControl extends React.Component {
+class FlavorForm extends React.Component {
     constructor(props) {
         super(props);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = {isLoggedIn: false};
+        this.state = {value: 'coconut'};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    handleLoginClick() {
-        this.setState({isLoggedIn: true});
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
     }
-    handleLogoutClick() {
-        this.setState({isLoggedIn: false});
+
+    handleSubmit(event) {
+        alert('Your favorite flavor is: ' + this.state.value);
+        event.preventDefault();
     }
+
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        let button;
-        if (isLoggedIn) {
-            button = <LogoutButton onClick={this.handleLogoutClick} />;
-        } else {
-            button = <LoginButton onClick={this.handleLoginClick} />;
-        }
         return (
-            <div>
-                <Greeting isLoggedIn={isLoggedIn} />
-                {button}
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Pick your favorite flavor:
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 }
 ReactDOM.render(
-    <LoginControl />,
+    <FlavorForm />,
     document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
